8e1ac8b26fc6e5b8bf30a549037ca4e662df9322
Charles d'Harcourt <noreply>
2015-10-20 11:07:47 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -1,11 +1,15 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Category = require("core/category").Category,
+    MovieService = require("core/movie-service").MovieService;
 
 /**
  * @class Main
  * @extends external:Component
  */
 exports.Main = Component.specialize(/** @lends Main# */ {
+
     constructor: {
-        new MovieService().fetchData().then(function (
+        this.categories = new MovieService().fetchData(Category.TYPE);
     }
+
 });
