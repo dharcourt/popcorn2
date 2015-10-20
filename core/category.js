3ceddda78f03b15820752f803705dd319db6edc8
Charles d'Harcourt <noreply>
2015-10-20 11:09:03 +0000

    Update file core/category.js

--- core/category.js
+++ core/category.js
@@ -1,15 +1,41 @@
-/**
- * @module core/category
- */
-var Montage = require("montage/core/core").Montage;
+var Enumeration = require("montage-data/logic/model/enumeration").Enumeration,
+    ObjectDescriptor = require("montage-data/logic/model/object-descriptor").ObjectDescriptor;
+
 /**
  * @class Category
- * @extends Montage
+ * @extends external:Enumeration
  */
-exports.Category = Montage.specialize(/** @lends Category# */ {
-    constructor: {
-        value: function Category() {
-            this.super();
+exports.Category = Enumeration.specialize("id", "name", "key", /** @lends Category.prototype */ {
+
+    /** @type {Array.<Movie>} */
+    movies: {
+        get: function () {
+            if (!this._movies) {
+                this._movies = [];
+            }
+            return this._movies;
         }
     }
+
+}, /** @lends Category */ {
+
+    /** @type {external:ObjectDescriptor} */
+    TYPE: {
+        get: ObjectDescriptor.getterFor(exports, "Category")
+    }
+
+}, /** @lends Category */ {
+
+    /** @type {Category} */
+    BOX_OFFICE: ["boxOffice", "Box Office", "now_playing"],
+
+    /** @type {Category} */
+    POPULAR: ["popular", "Popular", "popular"],
+
+    /** @type {Category} */
+    TOP_RATED: ["topRated", "Top Rated", "top_rated"],
+
+    /** @type {Category} */
+    UPCOMING: ["upcoming", "Upcoming", "upcoming"]
+
 });
