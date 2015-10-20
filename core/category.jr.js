4ffff01a69c4ff44855c635b5f2722dc06d6b242
Charles d'Harcourt <noreply>
2015-10-20 10:51:40 +0000

    Add module category.jr

---
+++ core/category.jr.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/category.jr
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Category.jr
+ * @extends Montage
+ */
+exports.Category.jr = Montage.specialize(/** @lends Category.jr# */ {
+    constructor: {
+        value: function Category.jr() {
+            this.super();
+        }
+    }
+});
