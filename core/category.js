701ae61748fc062387a23b716064ecfe0c03f2e2
Charles d'Harcourt <noreply>
2015-10-20 10:52:09 +0000

    Add module category

---
+++ core/category.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/category
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class Category
+ * @extends Montage
+ */
+exports.Category = Montage.specialize(/** @lends Category# */ {
+    constructor: {
+        value: function Category() {
+            this.super();
+        }
+    }
+});
