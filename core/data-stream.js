33344ddfe213b2fb767ccddf9ddd68f8b60318bf
Charles d'Harcourt <noreply>
2015-10-20 10:52:29 +0000

    Add module data-stream

---
+++ core/data-stream.js
@@ -0,0 +1,15 @@
+/**
+ * @module core/data-stream
+ */
+var Montage = require("montage/core/core").Montage;
+/**
+ * @class DataStream
+ * @extends Montage
+ */
+exports.DataStream = Montage.specialize(/** @lends DataStream# */ {
+    constructor: {
+        value: function DataStream() {
+            this.super();
+        }
+    }
+});
