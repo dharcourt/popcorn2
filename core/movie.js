042ebbc92a9dc6b860bb40ceb9f058e3934945c4
Charles d'Harcourt <noreply>
2015-10-20 10:56:40 +0000

    Update component ui/main.reel/

--- core/movie.js
+++ core/movie.js
@@ -1,15 +1,52 @@
-/**
- * @module core/movie
- */
-var Montage = require("montage/core/core").Montage;
+var Montage = require("montage/core/core").Montage,
+    ObjectDescriptor = require("montage-data/logic/model/object-descriptor").ObjectDescriptor;
+
 /**
  * @class Movie
- * @extends Montage
+ * @extends external:Montage
  */
-exports.Movie = Montage.specialize(/** @lends Movie# */ {
-    constructor: {
-        value: function Movie() {
-            this.super();
-        }
+exports.Movie = Montage.specialize(/** @lends Movie.prototype */ {
+
+    /** @type {string} */
+    title: {
+        value: undefined
+    },
+
+    /** @type {string} */
+    description: {
+        value: undefined
+    },
+
+    /** @type {string} */
+    backdrop: {
+        value: undefined
+    },
+
+    /** @type {string} */
+    poster: {
+        value: undefined
+    },
+
+    /** @type {string} */
+    releaseData: {
+        value: undefined
+    },
+
+    /** @type {number} */
+    popularity: {
+        value: undefined
+    },
+
+    /** @type {number} */
+    score: {
+        value: undefined
     }
+
+}, /** @lends Movie */ {
+
+    /** @type {external:ObjectDescriptor} */
+    TYPE: {
+        get: ObjectDescriptor.getterFor(exports, "Movie")
+    }
+
 });
